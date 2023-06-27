c2e5e0401d07655ec2ab5fe93f95da65a8a128f9
Pedro Brochero <pedrobrochero@gmail.com>
2023-06-27 02:55:02 -0500

    Fixed foreground service launch.

--- lib/features/foreground_service/presentation/cubit/foreground_service_cubit.dart
+++ lib/features/foreground_service/presentation/cubit/foreground_service_cubit.dart
@@ -14,22 +14,20 @@ class ForegroundServiceCubit extends Cubit<ForegroundServiceState> {
     notificationTitle: 'Timer(s) running...',
     // notificationIcon: AndroidResource(
     //     name: 'background_icon'), // Default is ic_launcher from folder mipmap
+    notificationIcon: AndroidResource(name: 'background_icon'),
   );
 
   /// Adds a [CustomTimer] to the list of active timers.
   /// Enables background service if not already on.
   void addTimer(CustomTimer timer) async {
-    print('Adding timer ${timer.name}');
     final success =
         await FlutterBackground.initialize(androidConfig: _androidConfig);
     if (!success) {
-      print('Failed to initialize FlutterBackground');
       return;
     }
     emit(state.copyWith(activeTimers: state.activeTimers + [timer]));
     if (!FlutterBackground.isBackgroundExecutionEnabled) {
-      final result = await FlutterBackground.enableBackgroundExecution();
-      print('Background execution enabled: $result');
+      await FlutterBackground.enableBackgroundExecution();
     }
   }
 
@@ -37,8 +35,7 @@ class ForegroundServiceCubit extends Cubit<ForegroundServiceState> {
   /// Disables background service if no active timers remain.
   void removeTimer(CustomTimer timer) async {
     final newList = state.activeTimers.toList()..remove(timer);
-    if (newList.isEmpty) {
-      print('No active timers remaining, disabling background execution');
+    if (newList.isEmpty && FlutterBackground.isBackgroundExecutionEnabled) {
       await FlutterBackground.disableBackgroundExecution();
     }
     emit(state.copyWith(activeTimers: newList));
