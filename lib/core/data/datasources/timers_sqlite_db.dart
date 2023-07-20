35589f57bbf5c669a890233cf8951d866c3dcf49
Pedro Brochero <pedrobrochero@gmail.com>
2023-07-20 18:09:19 -0500

    Added favorite and timesStarted field to db.

--- lib/core/data/datasources/timers_sqlite_db.dart
+++ lib/core/data/datasources/timers_sqlite_db.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: leading_newlines_in_multiline_strings
+
 import 'dart:async';
 
 import 'package:sqflite/sqflite.dart';
@@ -9,7 +11,7 @@ class TimersSqliteProvider extends SqliteProvider {
   String get dbName => SqliteProvider.timersDb;
 
   @override
-  int get dbVersion => 1;
+  int get dbVersion => 2;
 
   @override
   FutureOr<void> onCreate(Database db, int version) async {
@@ -17,7 +19,15 @@ class TimersSqliteProvider extends SqliteProvider {
   }
 
   @override
-  FutureOr<void> onUpgrade(Database db, int currentV, int newV) {}
+  FutureOr<void> onUpgrade(Database db, int currentV, int newV) async {
+    if (currentV <= 1 && newV >= 2) {
+      await db.transaction((txn) async {
+        for (final query in TimersTable._upgradeQuery1) {
+          await txn.execute(query);
+        }
+      });
+    }
+  }
 }
 
 class TimersTable {
@@ -25,10 +35,28 @@ class TimersTable {
   static const id = 'id';
   static const name = 'name';
   static const duration = 'duration';
+  static const timesStarted = 'timesStarted';
+  static const isFavorite = 'isFavorite';
 
   static const _creationQuery = 'CREATE TABLE $tableName( '
       '$id TEXT NOT NULL PRIMARY KEY, '
       '$name TEXT NOT NULL, '
-      '$duration INTEGER NOT NULL '
+      '$duration INTEGER NOT NULL, '
+      '$timesStarted INTEGER NOT NULL DEFAULT 0, '
+      '$isFavorite INTEGER NOT NULL DEFAULT 0 '
       ')';
+
+  static const _upgradeQuery1 = [
+    'CREATE TABLE temp AS SELECT $id, $name, $duration FROM $tableName',
+    'DROP TABLE $tableName',
+    '''CREATE TABLE $tableName( 
+    $id TEXT NOT NULL PRIMARY KEY, 
+    $name TEXT NOT NULL, 
+    $duration INTEGER NOT NULL, 
+    $timesStarted INTEGER NOT NULL DEFAULT 0, 
+    $isFavorite INTEGER NOT NULL DEFAULT 0 )''',
+    '''INSERT INTO $tableName( $id, $name, $duration ) 
+    SELECT $id, $name, $duration FROM temp ''',
+    'DROP TABLE temp',
+  ];
 }
